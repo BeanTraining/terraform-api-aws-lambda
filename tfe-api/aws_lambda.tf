2336d51b201f41c9e6c8f5cd50e977224005fecb
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-17 20:46:25 +0700

    We actually do not need EFS for Lambda yet

--- tfe-api/aws_lambda.tf
+++ tfe-api/aws_lambda.tf
@@ -1,34 +1,8 @@
 # https://learn.hashicorp.com/tutorials/terraform/lambda-api-gateway?in=terraform/aws
 
-# EFS access point used by lambda file system
-resource "aws_efs_access_point" "access_point_for_lambda" {
-  file_system_id = aws_efs_file_system.efs_for_lambda.id
+# 
 
-  root_directory {
-    path = "/lambda"
-    creation_info {
-      owner_gid   = 1000
-      owner_uid   = 1000
-      permissions = "777"
-    }
-  }
-
-  posix_user {
-    gid = 1000
-    uid = 1000
-  }
-}
-
-# EFS file system
-resource "aws_efs_file_system" "efs_for_lambda" {
-}
-
-# Mount target connects the file system to the subnet
-resource "aws_efs_mount_target" "bean" {
-  file_system_id  = aws_efs_file_system.efs_for_lambda.id
-  subnet_id       = aws_subnet.subnet_for_lambda.id
-  security_groups = [aws_security_group.sg_for_lambda.id]
-}
+# 
 
 /*
 Each Lambda function must have an associated IAM role which dictates what access it has to other AWS services. 
@@ -54,16 +28,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 EOF
 }
-
-/*
-  We need to attach AWSLambdaVPCAccessExecutionRole policy to iam_for_lambda role so that
-lambda can function in a VPC.
- */
-resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
-
+ 
 data "aws_s3_bucket_object" "bean-notification" {
   bucket = "479284709538-${var.aws_region}-aws-lambda"
   key    = "terraform-api/latest/notification.zip"
@@ -91,25 +56,9 @@ resource "aws_lambda_function" "bean-notification" {
     }
   }
 
-
-  file_system_config {
-    # EFS file system access point ARN
-    arn = aws_efs_access_point.access_point_for_lambda.arn
-
-    # Local mount path inside the lambda function. Must start with '/mnt/'.
-    local_mount_path = "/mnt/efs"
-  }
-
-  vpc_config {
-    # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
-    subnet_ids         = [aws_subnet.subnet_for_lambda.id]
-    security_group_ids = [aws_security_group.sg_for_lambda.id]
-  }
-
   # Explicitly declare dependency on EFS mount target.
   # When creating or updating Lambda functions, mount target must be in 'available' lifecycle state.
   depends_on = [
-    aws_efs_mount_target.bean,
     aws_iam_role_policy_attachment.lambda_logs,
     aws_cloudwatch_log_group.bean-notification
   ]
